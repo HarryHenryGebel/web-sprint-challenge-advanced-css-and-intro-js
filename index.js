88f1d074ef1a642c30817e816d3abdf00203b2cf
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-12 13:20:39 -0400

    Complete task 5

--- index.js
+++ index.js
@@ -258,21 +258,21 @@ function get20s(artistArray) {
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
- * removeArtist removes an artist from the array at the index and console.logs the length of the remaining dataset.
+ * removeArtist removes an artist from the array at the index and console.logs
+ * the length of the remaining dataset.
  *
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.
  *
- * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.
+ * Note that sucessfully invoking this function multiple times without
+ * refreshing your browser will continuously remove artists from the
+ * array until there are none left. If you refresh your browser, the
+ * data will reset.
  */
-function removeArtist(/*code here*/) {
-    /* code here */
-}
-
-/**
-
-
-
+function removeArtist(artistArray, index) {
+    artistArray.splice(index, 1);
+    console.log(artistArray.length);
+ }
 
    /* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
